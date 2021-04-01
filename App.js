dd18efa56c392be92c15857ee8f0946f2a8519fd
SolMarina <sparraa17@gmail.com>
2021-04-01 17:03:09 -0300

    estilo casi listo faltan los iconos

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button} from 'react-native';
 import { Header } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import nutri from './assets/nutrialuslogo.png'
+import nutri from './assets/nutrialuslogo.png';
+import { DataTable} from 'react-native-paper';
 import useFetch from './Hooks/UseFetch';
-
 
 export default function App() {
 
@@ -18,34 +18,41 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-
       <SafeAreaProvider>
-
         <Header
           leftComponent={{ icon: 'menu', color: '#F87113' }}
           centerComponent={<Image style={{ width: 160, height: 25, marginBottom: 15, borderRadius: 5 }} source={nutri} />}
           rightComponent={{ icon: 'home', color: '#F87113' }}
           containerStyle={{ width: 380, backgroundColor: 'white' }}
         />
-
       </SafeAreaProvider>
-
-
       <View style={styles.centeredView} >
 
-        <Text>Nombre: {name}</Text>
-        <Text>Número de teléfono: {phone}</Text>
-        <Text>Email: {email}</Text>
+        <Text style={{ color: '#F87113' }}><h3 >Informacion Personal</h3></Text>
+
+        <DataTable style={styles.tableView}>
+          <DataTable.Row>
+            <DataTable.Cell><Text Icon="camera" style={{ color: '#264653'}}>Nombre:</Text></DataTable.Cell>
+            <DataTable.Cell><Text style={{ color: '#264653',fontSize: 13  }}>{name}</Text></DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row >
+            <DataTable.Cell><Text style={{ color: '#264653'}}>Teléfono:</Text></DataTable.Cell>
+            <DataTable.Cell ><Text style={{ color: '#264653',fontSize: 13  }}>{phone}</Text></DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell><Text style={{ color: '#264653'}}>Email:</Text></DataTable.Cell>
+            <DataTable.Cell ><Text style={{ color: '#264653',fontSize: 12  }} >{email}</Text></DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
 
         <Image
-          style={{ width: 100, height: 100, marginBottom: 15, borderRadius: 5 }}
+          style={{ width: 150, height: 150, marginTop: 10, borderRadius: 5, marginBottom: 10 }}
           source={image}
         />
 
-
         <Button
           title="Siguiente"
-          color="#f194ff"
+          color="#F87113"
           onPress={next}
         />
       </View>
@@ -56,8 +63,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
-
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -68,17 +73,21 @@ const styles = StyleSheet.create({
   centeredView: {
     display: 'flex',
     flexFlow: 'column',
-    width: 300,
+    width: 350,
     padding: 10,
-    boxShadow: 20, color: 'grey',
     backgroundColor: 'whitesmoke',
     alignItems: 'center',
     borderRadius: 10,
-    marginBottom: 250
+    marginBottom: 150,
+    shadowColor: 'grey',
+    shadowRadius: 15
+
 
   },
 
+  tableView: {
+    width: 350,
 
-
+  },
 
 });
